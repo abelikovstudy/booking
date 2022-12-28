a8191bff91feed5a9d5a2cf6c88170c4def14557
Belikov A <beli@sfedu.ru>
2022-12-29 00:08:45 +0300

    Added hotel registration

--- app/controllers/hotel_controller.rb
+++ app/controllers/hotel_controller.rb
@@ -1,5 +1,18 @@
 class HotelController < ApplicationController
     def index
-        @hotels = Hotel.all
+        @hotels = Hotel.all 
+    end
+
+    def new
+        @hotel = Hotel.new
+    end
+
+    def create
+        @hotel = Hotel.new hotel_params
+
+    end
+
+    def hotel_params
+        params.require(:hotel).permit(:title, :addres, :description, :price, :owner, :email, :phone)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get  '/hotels', to: 'hotel#index'
+  get  '/hotels/new', to: 'hotel#new'
+  post '/hotels', to: 'hotel#create'
   root 'pages#index'
 
 end
